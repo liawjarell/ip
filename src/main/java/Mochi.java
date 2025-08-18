0127c25393d85612973f6905bd32f42c476a8069
liawjarell <jarellliaw@gmail.com>
2025-08-18 20:21:47 +0800

    Rename Duke to Mochi
    Created new chatbot Mochi, added introduction message and buffer
    using underscores 40 characters long

---
+++ src/main/java/Mochi.java
@@ -0,0 +1,10 @@
+public class Mochi {
+    public static void main(String[] args) {
+        System.out.println("""
+                ________________________________________
+                Hello! I'm MOCHI
+                What can I do for you?
+                ________________________________________
+                Bye. Hope to see you again!""");
+    }
+}
