9e65953f68b1888bcb3c33f6edf5cc2161357f82
liawjarell <jarellliaw@gmail.com>
2025-08-18 20:48:03 +0800

    Mochi accept input
    Mochi is able to accept input and repeat the input back to the user.
    When user inputs "bye", the program terminates.
    
    Also created constants HELLO_MESSAGE and BYE_MESSAGE.

--- src/main/java/Mochi.java
+++ src/main/java/Mochi.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Mochi {
+
+    private static final String HELLO_MESSAGE = """
+        ________________________________________
+        Hello! I'm MOCHI
+        What can I do for you?
+        ________________________________________""";
+
+    private static final String BYE_MESSAGE = """
+        ________________________________________
+        Bye. Hope to see you again!
+        ________________________________________""";
+
     public static void main(String[] args) {
-        System.out.println("""
-                ________________________________________
-                Hello! I'm MOCHI
-                What can I do for you?
-                ________________________________________
-                Bye. Hope to see you again!""");
+
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println(HELLO_MESSAGE);
+
+
+        String input = myObj.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = myObj.nextLine();
+        }
+        System.out.println(BYE_MESSAGE);
     }
 }
