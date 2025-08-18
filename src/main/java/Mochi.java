b85a3ecf46ab5a11caae6806a11c11d0036e96b3
liawjarell <jarellliaw@gmail.com>
2025-08-18 21:19:59 +0800

    Add ability to store and display inputs
    Mochi takes in input and adds it to a list. If list is inputted,
    displays all contents of the list. Created function printList to help
    print out list.

--- src/main/java/Mochi.java
+++ src/main/java/Mochi.java
@@ -13,18 +13,44 @@ public class Mochi {
         Bye. Hope to see you again!
         ________________________________________""";
 
+    // Function to print out all contents in the list
+    private static void printList(String[] list) {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] == null) {
+                break;
+            }
+            System.out.printf("%d. %s%n", i+1, list[i]);
+        }
+    }
+
     public static void main(String[] args) {
 
+        // Variables
+        String[] inputs = new String[100];
+        int inputsCount = 0;
         Scanner myObj = new Scanner(System.in);
 
+        // Intro message
         System.out.println(HELLO_MESSAGE);
 
-
-        String input = myObj.nextLine();
-        while (!input.equals("bye")) {
-            System.out.println(input);
-            input = myObj.nextLine();
+        // Loop to get input
+        while (true) {
+            String input = myObj.nextLine();
+            if (input.equals("bye")) {
+                // Exit condition
+                break;
+            } else if (input.equals("list")) {
+                // print out inputs
+                printList(inputs);
+            } else {
+                // Add input to list
+                inputs[inputsCount] = input;
+                System.out.println("added: " + input);
+                inputsCount++;
+            }
         }
+
+        // Exit message
         System.out.println(BYE_MESSAGE);
     }
 }
